d9562a130a1835baa1f78dd3a30748a5aed126f7
shinseunguk <krdut1@gmail.com>
2024-09-08 11:16:56 +0900

    [PGM] - 입문문제, 옷가게 할인 받기

---
+++ 프로그래머스/코딩테스트 입문/옷가게 할인 받기.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ price:Int) -> Int {
+    if price >= 500000 {
+        return Int(Double(price) * 0.8)
+    } else if price >= 300000 {
+        return Int(Double(price) * 0.9)
+    } else if price >= 100000 {
+        return Int(Double(price) * 0.95)
+    } else {
+        return price
+    }
+}
