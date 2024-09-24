267b3c0be2c7766d6195c0c2a33c83629f402e05
seung uk <69791286+shinseunguk@users.noreply.github.com>
2024-09-24 12:44:37 +0900

    [level 0] Title: 가위 바위 보, Time: 0.08 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+func solution(_ rsp:String) -> String {
+    rsp.map {
+        switch $0 {
+            case "0": 
+            return "5"
+            
+            case "2":
+            return "0"
+            
+            case "5":
+            return "2"
+            
+            default:
+            return "0"
+        }
+    }.joined(separator:"")
+    
+}
