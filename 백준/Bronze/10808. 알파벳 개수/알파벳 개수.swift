471f0018f9f95120866809f1f4690f129a0a97bb
seung uk <69791286+shinseunguk@users.noreply.github.com>
2024-10-08 14:26:04 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.swift
@@ -0,0 +1,45 @@
+var result = ""
+var alphabet = [
+    "a": 0,
+    "b": 0,
+    "c": 0,
+    "d": 0,
+    "e": 0,
+    "f": 0,
+    "g": 0,
+    "h": 0,
+    "i": 0,
+    "j": 0,
+    "k": 0,
+    "l": 0,
+    "m": 0,
+    "n": 0,
+    "o": 0,
+    "p": 0,
+    "q": 0,
+    "r": 0,
+    "s": 0,
+    "t": 0,
+    "u": 0,
+    "v": 0,
+    "w": 0,
+    "x": 0,
+    "y": 0,
+    "z": 0
+]
+
+for element in readLine()!.map { String($0) } {
+    alphabet[element]! += 1
+}
+
+let sortedAlphabet = alphabet.sorted{ $0.0 < $1.0 }
+
+for (key, value) in sortedAlphabet {
+    if key == "z" {
+        result += String(value)
+    } else {
+        result += String(value) + " "
+    }
+}
+
+print(result)
