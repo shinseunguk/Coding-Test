935804446ad84615686c24399f145c35f22b15bf
seung uk <69791286+shinseunguk@users.noreply.github.com>
2024-10-09 02:44:40 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 4 ms, Memory: 69096 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.swift
@@ -0,0 +1,8 @@
+let schools = [
+    "NLCS": "North London Collegiate School",
+    "BHA": "Branksome Hall Asia",
+    "KIS": "Korea International School",
+    "SJA": "St. Johnsbury Academy"
+]
+
+print(schools[readLine()!]!)
