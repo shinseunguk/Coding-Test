e86ef053015bd91a80f5cd9213473fd8c83c3c8f
seung uk <69791286+shinseunguk@users.noreply.github.com>
2024-10-08 15:43:05 +0900

    [Bronze II] Title: 음계, Time: 4 ms, Memory: 69096 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.swift
@@ -0,0 +1,12 @@
+let ascending = "1 2 3 4 5 6 7 8"
+let descending = "8 7 6 5 4 3 2 1"
+
+let input = readLine()!
+
+if input == ascending {
+    print("ascending")
+} else if input == descending {
+    print("descending")
+} else {
+    print("mixed")
+}
