3c97a1ed2e863c7a64f04cda46d3a83730403984
seung uk <69791286+shinseunguk@users.noreply.github.com>
2024-10-09 22:54:05 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 8 ms, Memory: 69096 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.swift
@@ -0,0 +1,14 @@
+switch readLine()! {
+case "M":
+    print("MatKor")
+case "W":
+    print("WiCys")
+case "C":
+    print("CyKor")
+case "A":
+    print("AlKor")
+case "$":
+    print("$clear")
+default:
+    break
+}
