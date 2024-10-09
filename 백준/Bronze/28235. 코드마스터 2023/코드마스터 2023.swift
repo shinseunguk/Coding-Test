180cd571fa05b96442ad6daef6d21e59f453245a
seung uk <69791286+shinseunguk@users.noreply.github.com>
2024-10-09 22:49:43 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 4 ms, Memory: 69096 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.swift
@@ -0,0 +1,12 @@
+switch readLine()! {
+case "SONGDO":
+    print("HIGHSCHOOL")
+case "CODE":
+    print("MASTER")
+case "2023":
+    print("0611")
+case "ALGORITHM":
+    print("CONTEST")
+default:
+    break
+}
