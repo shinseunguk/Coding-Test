c622d52aa2922ccd44ef79385d5e788b67ff01a9
seung uk <69791286+shinseunguk@users.noreply.github.com>
2024-10-14 15:30:47 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.swift
@@ -0,0 +1,13 @@
+let classes = [
+    "Algorithm": "204",
+    "DataAnalysis": "207",
+    "ArtificialIntelligence": "302",
+    "CyberSecurity": "B101",
+    "Network": "303",
+    "Startup": "501",
+    "TestStrategy": "105"
+]
+
+for _ in 0 ..< Int(readLine()!)! {
+    print(classes[readLine()!]!)
+}
