61ff1aeaf3b26c977a58002ef2912fb906ce8d50
seung uk <69791286+shinseunguk@users.noreply.github.com>
2024-10-14 15:44:42 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 4 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.swift
@@ -0,0 +1,18 @@
+switch Int(readLine()!)! {
+case 380 ..< 425:
+    print("Violet")
+case 425 ..< 450:
+    print("Indigo")
+case 450 ..< 495:
+    print("Blue")
+case 495 ..< 570:
+    print("Green")
+case 570 ..< 590:
+    print("Yellow")
+case 590 ..< 620:
+    print("Orange")
+case 620 ... 780:
+    print("Red")
+default:
+    break
+}
